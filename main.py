d607b638d898bbeaa230a95d72365b0799145bda
Mikaela Baluyot <mikaebal@gmail.com>
2025-03-19 00:35:15 -0700

    Removed initial commit message

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from adagrams.ui_helper import *
 from adagrams.game import draw_letters, uses_available_letters, score_word, get_highest_word_score
 
-# initial commit message 
+
 def wave_1_run_game():
     display_welcome_message()
     game_continue = True
