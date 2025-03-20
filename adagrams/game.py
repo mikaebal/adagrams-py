d8422f80a2f96ba8ce628edf2d0b29b42656443b
Mikaela Baluyot <mikaebal@gmail.com>
2025-03-19 21:52:21 -0700

    Completed Wave 3

--- adagrams/game.py
+++ adagrams/game.py
@@ -73,7 +73,32 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+
+    score_chart = {
+        'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+        'D': 2, 'G': 2,
+        'B': 3, 'C': 3, 'M': 3, 'P': 3,
+        'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+        'K': 5,
+        'J': 8, 'X': 8,
+        'Q': 10, 'Z': 10
+    }
+
+    if word == "":
+        return 0
+
+    total_score = 0
+
+    for letter in word:
+        letter = letter.upper()
+        if letter in score_chart:
+            total_score += score_chart[letter]
+
+    if len(word) >= 7:
+        total_score += 8
+
+    return total_score
+
 
 def get_highest_word_score(word_list):
     pass
